620757462886dadb0170541d82e913217b1377a7
delphidabbler <delphidabbler@4b90ff07-bfed-21a8-1d1e-447df33141ff>
2010-10-13 11:31:21 +0000

    cbview: * Version 1.4.0 Release.

---
+++ cbview/release-1.4.0/DevTools/Release.bat
@@ -0,0 +1,41 @@
+@rem ---------------------------------------------------------------------------
+@rem Clipboard Viewer Component.
+@rem
+@rem Script used to create zip file containing release files.
+@rem
+@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2008-2010.
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
+
+@echo off
+
+setlocal
+
+set ReleaseDir=Release
+set OutFile=%ReleaseDir%\dd-cbview.zip
+set HelpDir=Help
+set DocsDir=Docs
+set DemoDir=Demo
+
+cd .\..
+
+if exist %ReleaseDir% rmdir /S /Q %ReleaseDir%
+mkdir %ReleaseDir%
+
+zip %OutFile% -j -9 PJCBView.pas
+zip %OutFile% -j -9 PJCBView.dcr
+
+zip %OutFile% -j -9 %HelpDir%\PJCBView.hlp
+zip %OutFile% -j -9 %HelpDir%\PJCBView.als
+
+zip %OutFile% -j -9 %DocsDir%\ChangeLog.txt
+zip %OutFile% -j -9 %DocsDir%\MPL.txt
+zip %OutFile% -j -9 %DocsDir%\ReadMe.htm
+zip %OutFile% -j -9 %DocsDir%\Wiki.URL
+
+zip %OutFile% -r -9 %DemoDir%\*.* -x *.svn\*
+
+endlocal
