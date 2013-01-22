c62241bc39bca30b7519d249ef0e0e508df11445
delphidabbler <delphidabbler@4b90ff07-bfed-21a8-1d1e-447df33141ff>
2013-01-22 16:35:42 +0000

    md5: * Version 1.1.0 Release.

---
+++ md5/release-1.1.0/DevTools/Release.bat
@@ -0,0 +1,38 @@
+@rem ---------------------------------------------------------------------------
+@rem MD5 Message Digest Unit.
+@rem
+@rem Script used to create zip file containing release files.
+@rem
+@rem Any copyright in this file is dedicated to the Public Domain.
+@rem http://creativecommons.org/publicdomain/zero/1.0/
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
+@echo off
+
+setlocal
+
+set ReleaseDir=Release
+set OutFile=%ReleaseDir%\dd-md5.zip
+set DocsDir=Docs
+set TestDir=Test
+
+cd .\..
+
+if exist %ReleaseDir% rmdir /S /Q %ReleaseDir%
+mkdir %ReleaseDir%
+
+zip %OutFile% -j -9 PJMD5.pas
+
+zip %OutFile% -j -9 %DocsDir%\ChangeLog.txt
+zip %OutFile% -j -9 %DocsDir%\License.txt
+zip %OutFile% -j -9 %DocsDir%\MD5-Notice.txt
+zip %OutFile% -j -9 %DocsDir%\MPL-2.txt
+zip %OutFile% -j -9 %DocsDir%\ReadMe.htm
+zip %OutFile% -j -9 %DocsDir%\Documentation.URL
+
+zip %OutFile% -r -9 %TestDir%\*.* -x *.svn\*
+
+endlocal
