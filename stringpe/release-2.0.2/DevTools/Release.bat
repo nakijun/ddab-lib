433bfc43bb0138d17ca4d99fc26cdd87325a4355
delphidabbler <delphidabbler@4b90ff07-bfed-21a8-1d1e-447df33141ff>
2013-11-01 02:33:14 +0000

    stringpe: * Version 2.0.2 Release

---
+++ stringpe/release-2.0.2/DevTools/Release.bat
@@ -0,0 +1,34 @@
+@rem ---------------------------------------------------------------------------
+@rem Extended String Property Editor
+@rem
+@rem Script used to create zip file containing release files.
+@rem
+@rem Any copyright in this file is dedicated to the Public Domain.
+@rem http://creativecommons.org/publicdomain/zero/1.0/
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
+@echo off
+
+setlocal
+
+set ReleaseDir=Release
+set OutFile=%ReleaseDir%\dd-stringpe.zip
+set DocsDir=Docs
+
+cd .\..
+
+if exist %ReleaseDir% rmdir /S /Q %ReleaseDir%
+mkdir %ReleaseDir%
+
+zip %OutFile% -j -9 PJStringPE.pas
+zip %OutFile% -j -9 PJStringPE.dfm
+
+zip %OutFile% -j -9 %DocsDir%\ChangeLog.txt
+zip %OutFile% -j -9 %DocsDir%\MPL-2.txt
+zip %OutFile% -j -9 %DocsDir%\ReadMe.htm
+zip %OutFile% -j -9 %DocsDir%\DocumentationWiki.URL
+
+endlocal
