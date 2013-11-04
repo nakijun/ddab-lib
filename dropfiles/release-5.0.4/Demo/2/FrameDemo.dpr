a0e65cdedfac55133114914f3d4b68495296674c
delphidabbler <delphidabbler@4b90ff07-bfed-21a8-1d1e-447df33141ff>
2013-11-04 13:50:41 +0000

    dropfiles: * Version 5.0.4 Release.

---
+++ dropfiles/release-5.0.4/Demo/2/FrameDemo.dpr
@@ -0,0 +1,40 @@
+{ 
+ * FrameDemo.dpr
+ *
+ * Project file for demo program that demonstrates use of Drop Files Components
+ * with frames.
+ *
+ * $Rev$
+ * $Date$
+ *
+ * Any copyright in this file is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+}
+
+
+program FrameDemo;
+
+{$UNDEF DELPHIXE2ANDUP}
+{$IFDEF CONDITIONALEXPRESSIONS}
+  {$IF CompilerVersion >= 23.0} // Delphi XE2
+    {$DEFINE DELPHIXE2ANDUP}
+  {$IFEND}
+{$ENDIF}
+
+uses
+  {$IFNDEF DELPHIXE2ANDUP}
+  Forms,
+  {$ELSE}
+  Vcl.Forms,
+  {$ENDIF}
+  FmFrameDemo in 'FmFrameDemo.pas' {FmMain},
+  FrDemo in 'FrDemo.pas' {Frame1: TFrame};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.Title := 'DelphiDabbler Frame Drop Files Demo';
+  Application.CreateForm(TFmMain, FmMain);
+  Application.Run;
+end.
