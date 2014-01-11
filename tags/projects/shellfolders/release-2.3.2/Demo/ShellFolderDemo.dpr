d01cb3e3cc4895fc28847456d6e0b98a38bd11d3
delphidabbler <delphidabbler@4b90ff07-bfed-21a8-1d1e-447df33141ff>
2014-01-11 16:30:34 +0000

    shellfolders: * Version 2.3.2 Release.

---
+++ tags/projects/shellfolders/release-2.3.2/Demo/ShellFolderDemo.dpr
@@ -0,0 +1,40 @@
+{
+ * ShellFolderDemo.dpr
+ *
+ * Project file for Shell Folders Unit demo program.
+ *
+ * $Rev$
+ * $Date$
+ *
+ * Any copyright in this file is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+}
+
+
+program ShellFolderDemo;
+
+// Minimum compiler for this project is Delphi 7.
+{$UNDEF RTLNameSpaces}
+{$IF CompilerVersion >= 24.0} // Delphi XE3 and later
+  {$LEGACYIFEND ON}  // NOTE: this must come before all $IFEND directives
+{$IFEND}
+{$IF CompilerVersion >= 23.0} // Delphi XE2 and later
+  {$DEFINE RTLNameSpaces}
+{$IFEND}
+
+uses
+  {$IFNDEF RTLNameSpaces}
+  Forms,
+  {$ELSE}
+  Vcl.Forms,
+  {$ENDIF}
+  ShellFolderDemoForm in 'ShellFolderDemoForm.pas' {Form1};
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.Title := 'Shell Folders Demo';
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
