86ef06df084dd6bf6edb26d43ce64a151f835a52
delphidabbler <delphidabbler@4b90ff07-bfed-21a8-1d1e-447df33141ff>
2014-01-16 02:20:56 +0000

    resfile: * Version 1.1.0 Release.

---
+++ tags/projects/resfile/release-1.1.0/DevTools/Release.bat
@@ -0,0 +1,31 @@
+@rem ---------------------------------------------------------------------------
+@rem Script used to create zip file containing Resource File Unit release files.
+@rem
+@rem Any copyright in this file is dedicated to the Public Domain.
+@rem http://creativecommons.org/publicdomain/zero/1.0/
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
+@echo off
+
+setlocal
+
+set ReleaseDir=Release
+set OutFile=%ReleaseDir%\dd-resfile.zip
+set DocsDir=Docs
+
+cd .\..
+
+if exist %ReleaseDir% rmdir /S /Q %ReleaseDir%
+mkdir %ReleaseDir%
+
+zip %OutFile% -j -9 PJResFile.pas
+
+zip %OutFile% -j -9 %DocsDir%\ReadMe.htm
+zip %OutFile% -j -9 %DocsDir%\ChangeLog.txt
+zip %OutFile% -j -9 %DocsDir%\MPL-2.txt
+zip %OutFile% -j -9 %DocsDir%\Documentation.url
+
+endlocal
